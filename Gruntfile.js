e739da97d2013822986e41820252b468b99ab8aa
alexyork3 <durt2701@gmail.com>
2017-08-18 20:09:23 +0300

    Minify CSS and update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,23 @@ module.exports = function (grunt) {
 				src: "gf.js",
 				dest: "dist/gf.min.js"
 			}
+		},
+		cssmin: {
+			options: {
+				mergeIntoShorthands: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					"dist/gf.min.css": ["gf.css"]
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
 
-	grunt.registerTask("default", ["uglify"]);
+	grunt.registerTask("default", ["uglify", "cssmin"]);
 
 }
